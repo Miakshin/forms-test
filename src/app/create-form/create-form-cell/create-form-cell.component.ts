48189d5326c1bb271ad21132a21ffc6a6b176f0c
Miakshin <vlad.miakshin@gmail.com>
2018-03-27 12:32:17 +0300

    added some styles

--- src/app/create-form/create-form-cell/create-form-cell.component.ts
+++ src/app/create-form/create-form-cell/create-form-cell.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class CreateFormCellComponent {
 
   type: string;
+  title: string;
 
   constructor() {
     this.type = "string";
@@ -17,6 +18,12 @@ export class CreateFormCellComponent {
   onHendleType(ev){
     console.log(this.type);
     this.type = ev.target.value;
+  }
+  addDDI(ev){
+    let input = document.createElement("input");
+    input.className = "drop-down-items__input";
+    console.log(ev.target.parentNode)
+    ev.target.parentNode.insertBefore(input,ev.target)
   }
 
 }
